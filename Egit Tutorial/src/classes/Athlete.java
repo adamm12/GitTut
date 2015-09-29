5177088955c80cb6aca4b98b93453947c451947b
Adam <adam_m1@hotmail.ca>
2015-09-29 16:41:03 -0700

    Initial checkin

---
+++ Egit Tutorial/src/classes/Athlete.java
@@ -0,0 +1,40 @@
+package classes;
+
+public class Athlete {
+	
+	private String FIRSTNAME;
+	private String LASTNAME;
+	private int jerseyNum;
+	private String sport;
+	
+	public Athlete(String firstname, String lastname, String sport) {
+		// TODO Auto-generated constructor stub
+		FIRSTNAME = firstname;
+		LASTNAME = lastname;
+		this.sport = sport;
+		jerseyNum = 0;
+	}
+	
+
+    public void setSport(String sport) {
+    	this.sport = sport;
+    }
+    
+    public void setJerseyNum(int num) {
+    	jerseyNum = num;
+	}
+    
+    public int getJersey() {
+		return jerseyNum;
+	}
+    
+    public String getName() {
+		return FIRSTNAME + " " + LASTNAME;
+	}
+    
+    public String getSport() {
+		return sport;
+	}
+    
+    
+}
